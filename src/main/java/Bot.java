7694f72ff16a1dff95cff95cd72e550b44073426
unknown <unknown.com>
2020-07-28 16:59:55 +0100

    just a test

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,7 +1,7 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class Bot extends TelegramLongPollingBot {
+public class Botty extends TelegramLongPollingBot {
 
 
     public void onUpdateReceived(Update update) {
