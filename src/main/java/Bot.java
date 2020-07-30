54baaad98ece97f158e04971ad4ee7e8b81f7ab2
unknown <unknown.com>
2020-07-30 12:21:24 +0100

    added server up messages

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,6 +1,9 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,8 +17,10 @@ public class Bot extends TelegramLongPollingBot {
 
         if (SignalCheck(msg,buy)) {
             System.out.print("placing buy order"); //hand over to market-maker
+            sendMsg("566251065", "\uD83E\uDD16 placing buy order");
         } else if (SignalCheck(msg,sell)) {
             System.out.print("placing sell order");
+            sendMsg("566251065", "\uD83E\uDD16 placing sell order");
         }
     }
 
@@ -31,10 +36,23 @@ public class Bot extends TelegramLongPollingBot {
 
 
     /**
-     * Method sends Heartbeat to the bot to confirm server is still up.
+     * Method for creating a message and sending it.
+     * @param chatId chat id
+     * @param s The String that you want to send as a message.
      */
+    public synchronized void sendMsg(String chatId, String s) {
 
-    // complete
+        SendMessage execute = new SendMessage();
+        execute.enableMarkdown(true);
+        execute.setChatId(chatId);
+        execute.setText(s);
+        try {
+            execute(execute);
+        } catch (TelegramApiException e) {
+            Logger log = null;
+            log.log(Level.SEVERE, "Exception: ", e.toString());
+        }
+    }
 
     /**
      * Returns bot name.
--- src/main/java/Heartbeat.java
+++ src/main/java/Heartbeat.java
@@ -1,10 +1,14 @@
-public class Heartbeat implements Runnable{
+public class Heartbeat implements Runnable {
 
+    // Method sends show server up message to Telegram
     public void run() {
+        Bot botObject = new Bot();
+
         while (true) {
-            System.out.println("working master :pray:");
+            botObject.sendMsg("566251065", "\uD83E\uDD16 waiting");
+
             try {
-                Thread.sleep(3000);
+                Thread.sleep(3600000); //sleep for 60 minutes
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
